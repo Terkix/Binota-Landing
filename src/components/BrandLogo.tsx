ff578c27e47a02a3742b3de93642aa64408d7742
Terkix <terkix.eth@proton.me>
2026-04-23 13:23:48 -0600

    refactor: Update branding assets and add B1 logo
    - Add b1.svg logo asset
    - Update favicon.svg
    - Update BrandLogo component
    - Remove unused cursor assets (Arrow, Forbidden, Hourglass, Questionmark, Text)

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -4,7 +4,7 @@ export default function BrandLogo() {
   return (
     <div className="flex max-w-[1440px] justify-between items-center mb-48">
       <div className="flex">
-        <Image width={23} height={44} src="/assets/img/logo.svg" alt="B1 Logo" />
+        <Image width={46} height={88} src="/assets/img/logo.svg" alt="B1 Logo" />
       </div>
       <div className="flex">
         <Image width={144} height={144} src="/assets/img/Wordmark.svg" alt="Binota Wordmark" />
